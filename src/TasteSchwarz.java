646fd9414d6c84e93996d2badf09ce97fcf059a8
Jartreg <jartreg@users.noreply.github.com>
2018-02-18 14:28:07 +0100

    übrige Klassen kommentieren

--- src/TasteSchwarz.java
+++ src/TasteSchwarz.java
@@ -1,3 +1,9 @@
+/**
+ * Eine schwarze Taste
+ *
+ * Es werden verschiedene Klassen verwendet, um Greenfoot mitteilen zu können,
+ * dass schwarze Tasten immer über den weißen erscheinen sollen.
+ */
 public class TasteSchwarz extends Taste {
 	public TasteSchwarz(Note note, String key) {
 		super(note, key, "black");
